84801e82c2849d9249b8af840565dbe24ca1dbcb
tejasshilare <tejasshilare15@gmail.com>
2024-03-28 11:46:27 +0530

    New Update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
-import Body from "./components/Body"
+import Body from "./components/Body";
 import Header from "./components/Header";
 import Footerr from "./components/Footerr";
 import Texto from "./components/Texto";
 import Cards from "./components/Cards";
 
-
 function App() {
   return (
-   
-        <>
-        <Header/>
-        <Body/>
-        <Texto/>
-        <Cards/>
-        <Footerr/>
-        </>
+    <>
+      <Header />
+      <Body />
+      <Texto />
+      <Cards />
+      <Footerr />
+    </>
   );
 }
 
