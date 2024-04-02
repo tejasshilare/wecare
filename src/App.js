b7b58885d142a82123f86721551e36ce86060b60
tejasshilare <tejasshilare15@gmail.com>
2024-04-02 23:08:31 +0530

    pull

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import { createBrowserRouter as Routes,Route,Router } from "react-router-dom";
-import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-    
       <Header />
       <Home />
       <Footer />
-      
     </>
   );
 }
-
-
-
 
 export default App;
