9bd85a5db5a29a40f3eda75d17792536faebcd85
TejasMete001 <89549921+TejasMete001@users.noreply.github.com>
2024-04-16 23:51:50 +0530

    database connection

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,5 +1,27 @@
-import mongoose from "mongoose";
+import { type } from "@testing-library/user-event/dist/type";
+import  mongoose  from "mongoose";
 
 const userSchema = new mongoose.Schema({
-   
-})
+       FName:{
+         type:String,
+         require:true
+       },
+       LName:{
+           type:String,
+           require:true
+       },
+       Email:{
+           type:String,
+           require:true
+       },
+       PhoneNo:{
+         type:Number,
+         require:true
+       },
+       Password:{
+         type:String,
+         require:true
+       }
+},{timestamps:true});
+
+export const User = mongoose.model("User",userSchema);
