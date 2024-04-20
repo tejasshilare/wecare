8f70a5ae5228e3410973324eaf6bb331d75210a0
tejasshilare <tejasshilare15@gmail.com>
2024-04-20 11:59:27 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { useState } from "react";
 import weblg from "../assets/weblg.png";
 import { Link } from "react-router-dom";
+import usericon from "../assets/usericon.svg";
 const Navbar = () => {
   const [nav, setNav] = useState(true);
 
@@ -22,28 +23,39 @@ const Navbar = () => {
             Home
           </li>
         </Link>
-
         <Link to="/Donation">
           <li className="p-4  hover:bg-[#D3D3D3] transition-colors duration-300 cursor-pointer sm:hover:bg-gray-300 md:hover:bg-gray-400 lg:hover:font-2xl">
             Donation
           </li>
         </Link>
-
         <Link to="/about">
           <li className="p-4  hover:bg-[#D3D3D3] transition-colors duration-300 cursor-pointer sm:hover:bg-gray-300 md:hover:bg-gray-400 lg:hover:font-2xl">
             About
           </li>
         </Link>
-
         <Link to="/Contact">
-          <li className="p-4  hover:bg-[#D3D3D3] transition-colors duration-300 cursor-pointer sm:hover:bg-gray-300 md:hover:bg-gray-400 lg:hover:font-2xl">
+          <li className="p-4 hover:bg-[#D3D3D3] transition-colors duration-300 cursor-pointer sm:hover:bg-gray-300 md:hover:bg-gray-400 lg:hover:font-2xl">
             Contact
           </li>
         </Link>
-
+        <Link to="/" className="pl-[10px]">
+          <li className="p-4 hover:bg-[#D3D3D3] transition-colors duration-300 cursor-pointer sm:hover:bg-gray-300 md:hover:bg-gray-400 lg:hover:font-2xl">
+            logout
+          </li>
+        </Link>
         <Link to="/signup" className="pl-[10px]">
+          <li className="p-4 text-green-500 font-medium hover:bg-[#D3D3D3] transition-colors duration-300 cursor-pointer rounded-full md:hover:bg-orange-300 lg:hover:font-2xl ">
+            Register
+          </li>
+        </Link>
+        <Link to="/" className="pl-[10px]">
+          <li className="w-[35px] h-[35px] mt-[8px]">
+            <img src={usericon} alt="" />
+          </li>
+        </Link>
+        <Link to="/login" className="pl-[10px]">
           <button className="w-[110px] h-[35px] mt-[7px] text-white  bg-orange-400 rounded-full hover:bg-red-500 text-center hidden sm:inline-block">
-            Login/signup
+            Login
           </button>
         </Link>
       </ul>
@@ -61,12 +73,12 @@ const Navbar = () => {
         <div className="flex justify-between">
           <img className="w-[110px] " src={weblg} alt={weblg} />
           <Link
-            to="/signup"
+            to="/login"
             onClick={handleNav}
             className=" my-auto mx-6 md:hidden inline-block "
           >
             <button className="w-[110px] h-[35px] text-white  bg-orange-400 rounded-full hover:bg-red-500 text-center ">
-              Login/signup
+              Login
             </button>
           </Link>
         </div>
@@ -90,6 +102,11 @@ const Navbar = () => {
           <Link to="/Contact" onClick={handleNav}>
             <li className="p-4 border-b border-gray-800 hover:bg-[#D3D3D3] transition-colors duration-300 cursor-pointer sm:hover:bg-gray-300 md:hover:bg-gray-400 lg:hover:font-2xl">
               Contact
+            </li>
+          </Link>
+          <Link to="/signup" onClick={handleNav}>
+            <li className="p-4 border-b border-gray-800 hover:bg-[#D3D3D3] transition-colors duration-300 cursor-pointer sm:hover:bg-gray-300 md:hover:bg-gray-400 lg:hover:font-2xl">
+              Register
             </li>
           </Link>
         </ul>
