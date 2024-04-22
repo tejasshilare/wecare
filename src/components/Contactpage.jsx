c2d38048ba639bf13192a29049f2a3f035076e55
tejasshilare <tejasshilare15@gmail.com>
2024-04-22 12:15:07 +0530

    ..

--- src/components/Contactpage.jsx
+++ src/components/Contactpage.jsx
@@ -15,12 +15,12 @@ function Contactpage() {
               <h2 class="text-lg font-extrabold">Email</h2>
               <ul class="mt-3">
                 <li class="flex items-center">
-                  <div class="bg-[#e6e6e6cf] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
+                  <div class="bg-ch-grey h-10 w-10 rounded-full flex items-center justify-center shrink-0">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="20px"
                       height="20px"
-                      fill="#007bff"
+                      fill="rgb(249 115 22 / var(--tw-text-opacity))"
                       viewBox="0 0 479.058 479.058"
                     >
                       <path
@@ -32,7 +32,7 @@ function Contactpage() {
                   <div
                     target="blank"
                     href="/"
-                    class="text-[#007bff] text-sm ml-3"
+                    class="text-orange-500 text-sm ml-3"
                   >
                     <small class="block">Mail</small>
                     <strong>support@wecare.com</strong>
@@ -43,12 +43,12 @@ function Contactpage() {
             <div class="mt-12">
               <h2 class="text-lg font-extrabold">Socials</h2>
               <ul class="flex mt-3 space-x-4">
-                <li class="bg-[#e6e6e6cf] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
+                <li class="bg-ch-grey h-10 w-10 rounded-full flex items-center justify-center shrink-0">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="20px"
                     height="20px"
-                    fill="#007bff"
+                    fill="rgb(249 115 22 / var(--tw-text-opacity))"
                     viewBox="0 0 24 24"
                   >
                     <path
@@ -57,12 +57,12 @@ function Contactpage() {
                     />
                   </svg>
                 </li>
-                <li class="bg-[#e6e6e6cf] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
+                <li class="bg-ch-grey h-10 w-10 rounded-full flex items-center justify-center shrink-0">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="20px"
                     height="20px"
-                    fill="#007bff"
+                    fill="rgb(249 115 22 / var(--tw-text-opacity))"
                     viewBox="0 0 511 512"
                   >
                     <path
@@ -71,12 +71,12 @@ function Contactpage() {
                     />
                   </svg>
                 </li>
-                <li class="bg-[#e6e6e6cf] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
+                <li class="bg-ch-grey h-10 w-10 rounded-full flex items-center justify-center shrink-0">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="20px"
                     height="20px"
-                    fill="#007bff"
+                    fill="rgb(249 115 22 / var(--tw-text-opacity))"
                     viewBox="0 0 24 24"
                   >
                     <path d="M12 9.3a2.7 2.7 0 1 0 0 5.4 2.7 2.7 0 0 0 0-5.4Zm0-1.8a4.5 4.5 0 1 1 0 9 4.5 4.5 0 0 1 0-9Zm5.85-.225a1.125 1.125 0 1 1-2.25 0 1.125 1.125 0 0 1 2.25 0ZM12 4.8c-2.227 0-2.59.006-3.626.052-.706.034-1.18.128-1.618.299a2.59 2.59 0 0 0-.972.633 2.601 2.601 0 0 0-.634.972c-.17.44-.265.913-.298 1.618C4.805 9.367 4.8 9.714 4.8 12c0 2.227.006 2.59.052 3.626.034.705.128 1.18.298 1.617.153.392.333.674.632.972.303.303.585.484.972.633.445.172.918.267 1.62.3.993.047 1.34.052 3.626.052 2.227 0 2.59-.006 3.626-.052.704-.034 1.178-.128 1.617-.298.39-.152.674-.333.972-.632.304-.303.485-.585.634-.972.171-.444.266-.918.299-1.62.047-.993.052-1.34.052-3.626 0-2.227-.006-2.59-.052-3.626-.034-.704-.128-1.18-.299-1.618a2.619 2.619 0 0 0-.633-.972 2.595 2.595 0 0 0-.972-.634c-.44-.17-.914-.265-1.618-.298-.993-.047-1.34-.052-3.626-.052ZM12 3c2.445 0 2.75.009 3.71.054.958.045 1.61.195 2.185.419A4.388 4.388 0 0 1 19.49 4.51c.457.45.812.994 1.038 1.595.222.573.373 1.227.418 2.185.042.96.054 1.265.054 3.71 0 2.445-.009 2.75-.054 3.71-.045.958-.196 1.61-.419 2.185a4.395 4.395 0 0 1-1.037 1.595 4.44 4.44 0 0 1-1.595 1.038c-.573.222-1.227.373-2.185.418-.96.042-1.265.054-3.71.054-2.445 0-2.75-.009-3.71-.054-.958-.045-1.61-.196-2.185-.419A4.402 4.402 0 0 1 4.51 19.49a4.414 4.414 0 0 1-1.037-1.595c-.224-.573-.374-1.227-.419-2.185C3.012 14.75 3 14.445 3 12c0-2.445.009-2.75.054-3.71s.195-1.61.419-2.185A4.392 4.392 0 0 1 4.51 4.51c.45-.458.994-.812 1.595-1.037.574-.224 1.226-.374 2.185-.419C9.25 3.012 9.555 3 12 3Z"></path>
@@ -90,31 +90,31 @@ function Contactpage() {
             <input
               type="text"
               placeholder="Name"
-              class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
+              class="w-full rounded-md py-2.5 px-4 border text-sm outline-[rgb(249 115 22 / var(--tw-text-opacity))]"
             />
             <input
               type="email"
               placeholder="Email"
-              class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
+              class="w-full rounded-md py-2.5 px-4 border text-sm outline-[rgb(249 115 22 / var(--tw-text-opacity))]"
             />
             <input
               type="phone"
               placeholder="Phone Number"
-              class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
+              class="w-full rounded-md py-2.5 px-4 border text-sm outline-[rgb(249 115 22 / var(--tw-text-opacity))]"
             />
             <input
               type="text"
               placeholder="Subject"
-              class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
+              class="w-full rounded-md py-2.5 px-4 border text-sm outline-[rgb(249 115 22 / var(--tw-text-opacity))]"
             />
             <textarea
               placeholder="Message"
               rows="6"
-              class="w-full rounded-md px-4 border text-sm pt-2.5 outline-[#007bff]"
+              class="w-full rounded-md px-4 border text-sm pt-2.5 outline-[rgb(249 115 22 / var(--tw-text-opacity))]"
             ></textarea>
             <button
               type="button"
-              class="text-white bg-[#007bff] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-2.5 w-full"
+              class="text-white bg-[rgb(249 115 22 / var(--tw-text-opacity))] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-2.5 w-full"
             >
               Send
             </button>
